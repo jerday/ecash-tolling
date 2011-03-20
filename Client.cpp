2ffd863ca870a1c8d1d39cb1f53aa7806b433633
Jeremy Day <jerday@gmail.com>
2011-03-20 11:48:30 -0700

    use montgomery reduction to improve runtime by 40%

--- Client.cpp
+++ Client.cpp
@@ -55,9 +55,9 @@ void Client::registration(double revealed_per_interval, int tags_each_reveal, in
 	SHA256_Init(&sha256);
 
 	double start = omp_get_wtime( );
-int tid, nthreads, chunk;
-chunk = 10;
-int i = 0;
+	int tid, nthreads, chunk;
+	chunk = 10;
+	int i = 0;
 
 	#pragma omp parallel default(shared) private(i) \
 		num_threads(32)
@@ -69,53 +69,51 @@ int i = 0;
 			printf("Number of threads = %d\n", nthreads);
 		}
 		printf("Thread %d starting...\n",tid);
-	#pragma omp for schedule(static)
-	for(i = 0; i < num_tags; i++) {
-		// For each ticket i, the user sets m = (H(i,r),H(t,s)) where r and
-		// s are random salts.
-//		printf ("Thread %d: current i = %d\n", tid, i);
+		#pragma omp for schedule(static)
+		for(i = 0; i < num_tags; i++) {
+			// For each ticket i, the user sets m = (H(i,r),H(t,s)) where r and
+			// s are random salts.
+	//		printf ("Thread %d: current i = %d\n", tid, i);
 
-		BN_CTX * bnCtx = BN_CTX_new();
+			BN_CTX * bnCtx = BN_CTX_new();
 
-		byte ir[48];
-		memcpy(ir,_i,32);
-		memcpy(ir+32,_r[i],16);
-		SHA256_Update(&sha256,ir,48);
-		SHA256_Final(_m[i],&sha256); // m = H(i,r)
-		byte ts[18]; // 2 + 16
-		memcpy(ts,_t[i%num_times],2);
-		memcpy(ts+2,_s[i],16);
-		SHA256_Update(&sha256,ts,48);
-		SHA256_Final(_m[i]+32,&sha256); // _m[i] = (H(i,r),H(t,s))
+			byte ir[48];
+			memcpy(ir,_i,32);
+			memcpy(ir+32,_r[i],16);
+			SHA256_Update(&sha256,ir,48);
+			SHA256_Final(_m[i],&sha256); // m = H(i,r)
+			byte ts[18]; // 2 + 16
+			memcpy(ts,_t[i%num_times],2);
+			memcpy(ts+2,_s[i],16);
+			SHA256_Update(&sha256,ts,48);
+			SHA256_Final(_m[i]+32,&sha256); // _m[i] = (H(i,r),H(t,s))
 
-//		printf ("Thread %d HERE1: current i = %d\n", tid, i);
-		//  The value c = x^e H(m) is sent to the server
-		byte H_m[32];
-		SHA256_Update(&sha256,_m[i],64);
-		SHA256_Final(H_m,&sha256);
+	//		printf ("Thread %d HERE1: current i = %d\n", tid, i);
+			//  The value c = x^e H(m) is sent to the server
+			byte H_m[32];
+			SHA256_Update(&sha256,_m[i],64);
+			SHA256_Final(H_m,&sha256);
 
-		BIGNUM * x = BN_new();
-		BN_rand_range(x,Server::get_n()); // generate random x less than n
-		// raise x^e
+			BIGNUM * x = BN_new();
+			BN_rand_range(x,Server::get_n()); // generate random x less than n
+			// raise x^e
 
-		BIGNUM * x_pow_e = BN_new();
-		BN_mod_exp(x_pow_e,x,Server::get_e(),Server::get_n(),bnCtx);
-//		printf ("Thread %d HERE2: current i = %d\n", tid, i);
+			BIGNUM * x_pow_e = BN_new();
+			BN_mod_exp(x_pow_e,x,Server::get_e(),Server::get_n(),bnCtx);
+	//		printf ("Thread %d HERE2: current i = %d\n", tid, i);
 
-		BIGNUM * bn_H_m = BN_new();
-		BN_bin2bn(H_m,32,bn_H_m);
+			BIGNUM * bn_H_m = BN_new();
+			BN_bin2bn(H_m,32,bn_H_m);
 
-		BIGNUM * c = BN_new();
-		BN_mod_mul(c,bn_H_m,x_pow_e,Server::get_n(),bnCtx);
+			BIGNUM * c = BN_new();
+			BN_mod_mul(c,bn_H_m,x_pow_e,Server::get_n(),bnCtx);
 
-		BIGNUM * gamma = Server::compute_gamma(c,bnCtx);
-		BN_div(_sigma[i],NULL,gamma,x,bnCtx);
+			BIGNUM * gamma = Server::compute_gamma(c,bnCtx);
+			BN_div(_sigma[i],NULL,gamma,x,bnCtx);
+		}
 	}
-	}
-
-double end = omp_get_wtime( );
-
-printf ("time spent in phase 2: %.16g\n", end - start); 
+	double end = omp_get_wtime( );
+	printf ("time spent in phase 2: %.16g\n", end - start);
 }
 
 void Client::reveal() {
