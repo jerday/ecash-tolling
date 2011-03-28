629a31705736f1196f60b833cf45fd586235a9c3
andy <andy@andy-laptop.(none)>
2011-03-27 22:42:29 -0400

    less memory leak

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char** argv)
 {
-	for (int i = 512; i < 2048; i *= 2) {
+//	for (int i = 1024; i < 2048 * 2; i *= 2) {
 		  system ("rm double_spending.db");
 		  system ("rm ecash-tolling.db");
 		Server::key_generation();
@@ -18,14 +18,14 @@ int main(int argc, char** argv)
     //}
     //c.registration(0.2,1,30/*30*/);
   		  //c.registration(0.2,1,i/*30*/);
-  		  c.registration(0.2,1,i/*30*/);
+  		  c.registration(1,1,30 * 10/*30*/);
 		    c.reveal(0.5);
  		   c.payment();
 		    printf ("Total communication cost = %d bytes\n", c.cc_bytes);
 		    printf ("Stored Bytes = %d\n", Server::bytes_stored);
 		    sqlite3_close(Server::db);
   		  sqlite3_close(Server::ds_db);
-	}
+//	}
     
     return 0;
 
